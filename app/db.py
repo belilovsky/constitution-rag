06392dcd5e94d8d757ce37fcc37093039c4a3429
belilovsky <a.belilovsky@gmail.com>
2026-03-13 01:17:11 +0500

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,17 +1,21 @@
+import os
+
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-DB = {
-    "host": "127.0.0.1",
-    "port": 55432,
-    "dbname": "constitution_rag",
-    "user": "constitution_rag",
-    "password": "ConstitutionRag_2026_Strong_Pass_Change_This",
-}
+
+def get_db_config():
+    return {
+        "host": os.getenv("DB_HOST", "127.0.0.1"),
+        "port": int(os.getenv("DB_PORT", "55432")),
+        "dbname": os.getenv("DB_NAME", "constitution_rag"),
+        "user": os.getenv("DB_USER", "constitution_rag"),
+        "password": os.getenv("DB_PASSWORD", "ConstitutionRag_2026_Strong_Pass_Change_This"),
+    }
 
 
 def get_conn():
-    return psycopg2.connect(**DB)
+    return psycopg2.connect(**get_db_config())
 
 
 def fetch_all(sql, params=None):
